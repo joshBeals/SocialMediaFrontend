ada114c3c84f0bdb3777aca7de91ee046b624034
JoshuaBeals <joshua@camelcase.co>
2023-05-10 20:48:46 -0400

    NavBar Done

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ function App() {
     const mode = useSelector((state) => state.mode);
     const theme = useMemo(() => createTheme(themeSettings(mode)), [mode]);
 
-    return (
+    return ( 
         <div className="app">
             <BrowserRouter>
                 <ThemeProvider theme={theme}>
